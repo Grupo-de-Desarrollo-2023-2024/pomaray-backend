22e30def047a0d9b725da54055f47700439e2df9
NombreAf <nahuelramirez20179@gmail.com>
2024-03-08 22:17:47 -0400

    cambios en la estructura

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', include('pomaray.urls'))
+    path('students/', include('pomaray.api.student'))
 ]
--- pomaray/api/student.py
+++ pomaray/api/student.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
-from pomaray import views
+from pomaray.views.student import StudentView
 
 router = routers.DefaultRouter()
-router.register(r'students', views.StudentView, 'students')
+router.register(r'students', StudentView, 'students')
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
