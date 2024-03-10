1e059d5b953d34ac9447b57101d7274478b38c27
NombreAf <nahuelramirez20179@gmail.com>
2024-03-10 16:27:49 -0400

    calificaion tecnica

--- pomaray/api/qualifications.py
+++ pomaray/api/qualifications.py
@@ -5,10 +5,10 @@ from pomaray.views.qualifications import academicQualificationView, tecnicQualif
 
 router = routers.DefaultRouter()
 router.register(r'academicQualification', academicQualificationView, 'academicQualification')
-router.register(r'tecnumQualificationView', tecnicQualificationView, 'tecnicQualification')
+router.register(r'tecnicQualification', tecnicQualificationView, 'tecnicQualification')
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
-    path('docs/', include_docs_urls(title='academicQualification API')),
-    path('docs/', include_docs_urls(title='tecnicQualification API'))
+    path('docs/academic/', include_docs_urls(title='academicQualification API')),
+    path('docs/tecnic/', include_docs_urls(title='tecnicQualification API'))
 ]
