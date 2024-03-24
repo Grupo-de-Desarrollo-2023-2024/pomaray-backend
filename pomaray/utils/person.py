f7759ceb2e0cded2e6255004208626985e1b1f4a
NombreAf <nahuelramirez20179@gmail.com>
2024-03-24 17:46:38 -0400

    campo fullname

--- pomaray/utils/person.py
+++ pomaray/utils/person.py
@@ -1,15 +1,22 @@
 from django.db import models
 from .enums import Gender
 
-
 class Person(models.Model):
     First_Name = models.CharField(max_length=100)
     Last_Name = models.CharField(max_length=100)
+    Full_Name = models.CharField(max_length=200, blank=True)
     Email = models.EmailField()
     Birthdate = models.DateTimeField()
     Gender = models.CharField(max_length=255, choices=Gender.choices)
     Address = models.TextField(max_length=250)
     Photo_URL = models.CharField(max_length=4000)
+    IsActive = models.BooleanField(default=True)  
+    def save(self, *args, **kwargs):
+        self.Full_Name = f"{self.First_Name} {self.Last_Name}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.Full_Name
 
     class Meta:
-        abstract = True  # Indica que esta clase no se debe crear como una tabla en la base de datos
+        abstract = True
